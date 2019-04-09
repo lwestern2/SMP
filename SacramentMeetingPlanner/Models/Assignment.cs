d3c3df854ca13455afa03123147a132f3751edbb
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-04-08 23:12:11 -0600

    Css & back to working system

--- SacramentMeetingPlanner/Models/Assignment.cs
+++ SacramentMeetingPlanner/Models/Assignment.cs
@@ -12,8 +12,8 @@ namespace SacramentMeetingPlanner.Models
         public int MeetingID { get; set; }
         public string assignment { get; set; }
 
-        public Member Member { get; set; }
-        public Assignment Assignments { get; set; }
+        //public Member Member { get; set; }
+        //public Assignment Assignments { get; set; }
 
 
     }
